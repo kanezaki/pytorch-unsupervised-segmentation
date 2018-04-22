d61b65c311199e457494873665f606fd14664d1a
Asako Kanezaki <kanezaki.asako@aist.go.jp>
2018-04-22 16:30:59 +0900

    not to share bn layers

--- demo.py
+++ demo.py
@@ -40,23 +40,25 @@ class MyNet(nn.Module):
     def __init__(self,input_dim):
         super(MyNet, self).__init__()
         self.conv1 = nn.Conv2d(input_dim, args.nChannel, kernel_size=3, stride=1, padding=1 )
+        self.bn1 = nn.BatchNorm2d(args.nChannel)
         self.conv2 = []
+        self.bn2 = []
         for i in range(args.nConv-1):
             self.conv2.append( nn.Conv2d(args.nChannel, args.nChannel, kernel_size=3, stride=1, padding=1 ) )
+            self.bn2.append( nn.BatchNorm2d(args.nChannel) )
         self.conv3 = nn.Conv2d(args.nChannel, args.nChannel, kernel_size=1, stride=1, padding=0 )
-        self.bn0 = nn.BatchNorm2d(args.nChannel)
-        self.bn1 = nn.BatchNorm2d(args.nChannel)
+        self.bn3 = nn.BatchNorm2d(args.nChannel)
 
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu( x )
-        x = self.bn0(x)
+        x = self.bn1(x)
         for i in range(args.nConv-1):
             x = self.conv2[i](x)
             x = F.relu( x )
-            x = self.bn0(x)
+            x = self.bn2[i](x)
         x = self.conv3(x)
-        x = self.bn1(x)
+        x = self.bn3(x)
         return x
 
 # load image
@@ -80,6 +82,7 @@ if use_cuda:
     model.cuda()
     for i in range(args.nConv-1):
         model.conv2[i].cuda()
+        model.bn2[i].cuda()
 model.train()
 loss_fn = torch.nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
